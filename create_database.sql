f11f452f63f1a2dd19843e53f509fd5b43d87b5f
Tobias <tobiasschiphorst@live.nl>
2016-11-05 22:35:55 +0100

    hotelname added in table hotels

--- create_database.sql
+++ create_database.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Oct 28, 2016 at 04:18 
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.23
+-- Host: 127.0.0.1
+-- Gegenereerd op: 05 nov 2016 om 21:34
+-- Serverversie: 5.7.14
+-- PHP-versie: 5.6.25
 
-
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,16 +17,16 @@
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `359861`
+-- Database: `mission-reisbureau`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ACCOUNT`
+-- Tabelstructuur voor tabel `account`
 --
 
-CREATE TABLE `ACCOUNT` (
+CREATE TABLE `account` (
   `email` varchar(20) NOT NULL,
   `PASSWORD` varchar(20) NOT NULL,
   `is_subscribed_to_newsletter` char(1) NOT NULL,
@@ -33,13 +34,23 @@ CREATE TABLE `ACCOUNT` (
   `CUSTOMER_customer_ID` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `account`
+--
+
+INSERT INTO `account` (`email`, `PASSWORD`, `is_subscribed_to_newsletter`, `travel_points`, `CUSTOMER_customer_ID`) VALUES
+('aapje@kees.nl', 'hallo', '1', 45, 1),
+('boobie@jaap.net', 'dagjesmen', '0', 0, 2),
+('juppp@beest.nl', 'jarigejet', '1', 0, 5),
+('klarinet@huis.be', 'mzuiek', '0', 367, 3);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `AIRPORT`
+-- Tabelstructuur voor tabel `airport`
 --
 
-CREATE TABLE `AIRPORT` (
+CREATE TABLE `airport` (
   `airportID` int(7) NOT NULL,
   `abbreviation` varchar(5) NOT NULL,
   `adress` varchar(20) NOT NULL,
@@ -48,13 +59,24 @@ CREATE TABLE `AIRPORT` (
   `phonenumber` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `airport`
+--
+
+INSERT INTO `airport` (`airportID`, `abbreviation`, `adress`, `zip`, `city`, `phonenumber`) VALUES
+(123, 'AMS', 'schiphollaan2', '1234AH', 'Amsterdam', 983747123),
+(391, 'KAK', 'typefout 7', '9999AA', 'stijlfout', 765434567),
+(432, 'BOB', 'Keesstraat 5', '8719AN', 'Bobbejaanland', 382739582),
+(837, 'GRN', 'eemshavenweg 1', '2345GH', 'Groningen', 505123134),
+(987, 'UHG', 'Vondellaan 2 ', '6481UH', 'Uhage', 876543212);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `BOOKING`
+-- Tabelstructuur voor tabel `booking`
 --
 
-CREATE TABLE `BOOKING` (
+CREATE TABLE `booking` (
   `bookingID` int(5) NOT NULL,
   `city` varchar(20) NOT NULL,
   `date_of_arrival` date NOT NULL,
@@ -62,13 +84,24 @@ CREATE TABLE `BOOKING` (
   `CUSTOMER_customer_ID` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `booking`
+--
+
+INSERT INTO `booking` (`bookingID`, `city`, `date_of_arrival`, `date_of_departure`, `CUSTOMER_customer_ID`) VALUES
+(2347, 'Athene', '2016-11-08', '2016-11-28', 2),
+(3415, 'Napels', '2016-10-12', '2016-10-28', 1),
+(3457, 'Sneek', '2016-11-02', '2016-11-22', 5),
+(23425, 'Rome', '2016-12-01', '2016-12-07', 4),
+(23497, 'Groningen', '2016-10-03', '2016-10-19', 3);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `BUSRESERVATION`
+-- Tabelstructuur voor tabel `busreservation`
 --
 
-CREATE TABLE `BUSRESERVATION` (
+CREATE TABLE `busreservation` (
   `reservationID` int(5) NOT NULL,
   `boarding_point` varchar(20) NOT NULL,
   `price` int(6) NOT NULL,
@@ -76,13 +109,24 @@ CREATE TABLE `BUSRESERVATION` (
   `boarding_point_return` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `busreservation`
+--
+
+INSERT INTO `busreservation` (`reservationID`, `boarding_point`, `price`, `BOOKING_bookingID`, `boarding_point_return`) VALUES
+(423, 'Groningen', 123, 2347, 'Kip'),
+(453, 'Groningen', 1235, 3457, 'Heerenveen'),
+(983, 'Groningen', 572, 3415, 'Leeuwarden'),
+(3454, 'Heerhugowaard', 895, 23425, 'Groningen'),
+(9827, 'Hulp', 999, 23497, 'Hel');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `CUSTOMER`
+-- Tabelstructuur voor tabel `customer`
 --
 
-CREATE TABLE `CUSTOMER` (
+CREATE TABLE `customer` (
   `customer_ID` int(5) NOT NULL,
   `last_name` varchar(20) NOT NULL,
   `first_name` varchar(20) DEFAULT NULL,
@@ -93,13 +137,24 @@ CREATE TABLE `CUSTOMER` (
   `phonenumber` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `customer`
+--
+
+INSERT INTO `customer` (`customer_ID`, `last_name`, `first_name`, `adress`, `zipcode`, `city`, `country`, `phonenumber`) VALUES
+(1, 'Janssen', 'Piet', 'Heereborg 158', '9736AH', 'Groningen', 'Nederland', 678547619),
+(2, 'Wieringa', NULL, 'Fietspompstraat 5', '4718GJ', 'Fietselstijn', 'Fietselgracht', 638562987),
+(3, 'visje', 'iglo', 'zeestraat 4', '1234BL', 'Atlantis', 'Atlantica', 737519736),
+(4, 'Baba', 'Ali', 'hooipolder', '2748UH', 'Bagdad', 'Duitsland', 472487623),
+(5, 'Yuri', 'M', 'Hulpstraat 1', '3784AT', 'Groningen', 'Friesland', 629384753);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `FLIGHTRESERVATION`
+-- Tabelstructuur voor tabel `flightreservation`
 --
 
-CREATE TABLE `FLIGHTRESERVATION` (
+CREATE TABLE `flightreservation` (
   `flightID` int(5) NOT NULL,
   `seat` int(4) NOT NULL,
   `is_less_mobile` char(1) DEFAULT NULL,
@@ -111,28 +166,51 @@ CREATE TABLE `FLIGHTRESERVATION` (
   `AIRPORT_airportID` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `flightreservation`
+--
+
+INSERT INTO `flightreservation` (`flightID`, `seat`, `is_less_mobile`, `allergies`, `BOOKING_bookingID`, `departure_time`, `arrival_time`, `price`, `AIRPORT_airportID`) VALUES
+(1, 1, '', '1', 3415, '2016-09-30', '2016-10-01', 24, 123),
+(11, 4, '', '', 3457, '2016-10-01', '2016-10-25', 76, 432),
+(523, 23, '1', NULL, 2347, '2016-10-04', '2016-10-05', 2345, 391),
+(2342, 35, '1', '', 23497, '2016-10-02', '2016-10-09', 555, 987),
+(9999, 55, '', '', 23425, '2016-10-05', '2016-10-12', 345, 837);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `HOTEL`
+-- Tabelstructuur voor tabel `hotel`
 --
 
-CREATE TABLE `HOTEL` (
+CREATE TABLE `hotel` (
   `hotelID` int(10) NOT NULL,
   `stars` int(5) DEFAULT NULL,
   `rating` int(3) NOT NULL,
   `adress` varchar(20) NOT NULL,
   `contact_person` varchar(20) NOT NULL,
-  `phone_number` int(10) NOT NULL
+  `phone_number` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `hotel`
+--
+
+INSERT INTO `hotel` (`hotelID`, `stars`, `rating`, `adress`, `contact_person`, `phone_number`, `name`) VALUES
+(55, 4, 9, 'Hooidoei 3', 'pikachu', 1827563287, 'Het zoentje'),
+(74, 5, 6, 'Mooispullaan 2', 'Piet jansen', 658398575, 'De lachende pul'),
+(99, 2, 4, 'schoeleweg 6', '7384AB', 62547616, 'Het biertje'),
+(242, 5, 10, 'Duurestraat 1', 'Dagobert Duck', 666666666, 'Hilton'),
+(546, 3, 8, 'Rensumaheerd 42', 'Ricardo', 57947410, 'Van der Valk');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ROOM`
+-- Tabelstructuur voor tabel `room`
 --
 
-CREATE TABLE `ROOM` (
+CREATE TABLE `room` (
   `roomID` int(5) NOT NULL,
   `beds` int(1) NOT NULL,
   `TYPE` varchar(20) NOT NULL,
@@ -142,62 +220,73 @@ CREATE TABLE `ROOM` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Gegevens worden geëxporteerd voor tabel `room`
+--
+
+INSERT INTO `room` (`roomID`, `beds`, `TYPE`, `price`, `HOTEL_hotelID`, `BOOKING_bookingID`) VALUES
+(11134, 1, 'luxe', 567, 55, 2347),
+(12345, 2, 'comfort', 500, 242, 23425),
+(64627, 2, 'Standard', 123, 74, 3415),
+(98765, 2, 'standard', 420, 546, 23497),
+(456324, 1, 'wedding', 600, 99, 3457);
+
+--
+-- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexes for table `ACCOUNT`
+-- Indexen voor tabel `account`
 --
-ALTER TABLE `ACCOUNT`
+ALTER TABLE `account`
   ADD PRIMARY KEY (`email`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `AIRPORT`
+-- Indexen voor tabel `airport`
 --
-ALTER TABLE `AIRPORT`
+ALTER TABLE `airport`
   ADD PRIMARY KEY (`airportID`),
   ADD UNIQUE KEY `airportID` (`airportID`);
 
 --
--- Indexes for table `BOOKING`
+-- Indexen voor tabel `booking`
 --
-ALTER TABLE `BOOKING`
+ALTER TABLE `booking`
   ADD PRIMARY KEY (`bookingID`),
   ADD UNIQUE KEY `bookingID` (`bookingID`);
 
 --
--- Indexes for table `BUSRESERVATION`
+-- Indexen voor tabel `busreservation`
 --
-ALTER TABLE `BUSRESERVATION`
+ALTER TABLE `busreservation`
   ADD PRIMARY KEY (`reservationID`),
   ADD UNIQUE KEY `reservationID` (`reservationID`);
 
 --
--- Indexes for table `CUSTOMER`
+-- Indexen voor tabel `customer`
 --
-ALTER TABLE `CUSTOMER`
+ALTER TABLE `customer`
   ADD PRIMARY KEY (`customer_ID`),
   ADD UNIQUE KEY `customer_ID` (`customer_ID`);
 
 --
--- Indexes for table `FLIGHTRESERVATION`
+-- Indexen voor tabel `flightreservation`
 --
-ALTER TABLE `FLIGHTRESERVATION`
+ALTER TABLE `flightreservation`
   ADD PRIMARY KEY (`flightID`),
   ADD UNIQUE KEY `flightID` (`flightID`);
 
 --
--- Indexes for table `HOTEL`
+-- Indexen voor tabel `hotel`
 --
-ALTER TABLE `HOTEL`
+ALTER TABLE `hotel`
   ADD PRIMARY KEY (`hotelID`),
   ADD UNIQUE KEY `hotelID` (`hotelID`);
 
 --
--- Indexes for table `ROOM`
+-- Indexen voor tabel `room`
 --
-ALTER TABLE `ROOM`
+ALTER TABLE `room`
   ADD PRIMARY KEY (`roomID`),
   ADD UNIQUE KEY `roomID` (`roomID`);
 
