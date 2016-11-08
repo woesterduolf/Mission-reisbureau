bcfb6daa191a8a77022160d4cfbd6d9fea4e7c2a
Tobias <tobiasschiphorst@live.nl>
2016-11-08 11:39:56 +0100

    booking concept layout

--- hanzeproject/includes/booking.php
+++ hanzeproject/includes/booking.php
@@ -8,7 +8,7 @@
 	
 	// filling the session with testdata
 	
-	$_SESSION['city'] = "Athene";
+	$_SESSION['city'] = "Athens";
 	$_SESSION['date_of_arrival'] = "2016-11-08";
 	$_SESSION['date_of_departure'] = "2016-11-28";
 	$_SESSION['hotel_name'] = "Het zoentje";
@@ -27,7 +27,11 @@
 	$roomPrice = $_SESSION['room_price'];
 	$busPrice = $_SESSION['bus_price'];
 	$flightPrice = $_SESSiON['flight_Price'];
-	$transport_type = $_SESSiON['transport_type'];
+	$transport_type = $_SESSION['transport_type'];
+
+	$getImageUrl = "../images/cities/" . strtolower($city) . ".jpg";
+	$getHotelImageUrl = "../images/hotels/" . strtolower($hotelName) . ".jpg";
+	$standardImage = "../images/hotels/standard.jpg";
 	
 	
 	//calculate room price roomprice * days
@@ -44,7 +48,9 @@
 	<link href="../css/bootstrap.css" rel='stylesheet' type='text/css' />
 	<script src="../js/jquery.min.js"></script>
 	<link href="../css/style.css" rel="stylesheet" type="text/css" media="all" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../css/thumbnail-gallery.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/image-overlay.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 <div class="container-fluid">
@@ -52,71 +58,108 @@
 		<div class="row">
 			<div class="col-md-12 text-center">
 				<h1>Your booking</h1>
+				<p class="lead">This is your dream holiday only 2 steps left.</p>
+				<hr>
 			</div>
 		</div>
-	
-		<table class="table">
-			<tr class="text-center">
-				<td><h3>General data</h3><br>
-				<p>Under you will find your information about date of arrival, date of departure and the city.</p>
-				</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td>Date of arrival:</td>
-				<td><?php echo $arrivalDate; ?></td>
-			</tr>
-			<tr>
-				<td>Date of departure:</td>					
-				<td><?php echo $departureDate; ?></td>
-			</tr>
-			<tr>
-				<td>City:</td>
-				<td><?php echo $city; ?></td>
-			</tr>
-			<tr class="text-center">
-				<td>
-					<h3>Hotel information</h3>
+		<br>
+		<div class="row">
+			<div class="col-md-6 thumb">
+                <div class="hovereffect">
+                    <a class="thumbnail">
+                        <img class="img-responsive" src="<?php echo $getImageUrl; ?>" alt="<?php echo $city; ?>">
+                    </a>
+                    <div class="overlay">
+                        <h2><?php echo $city; ?></h2>
+                    </div>
+                </div>
+            </div>
+			<div class="col-md-6">
+				<div class="text-left">
+					<h2>General data</h2>
+					<p>Under you will find your information about date of arrival, date of departure and the city.</p>
+				</div>
+				<br>
+				<table class="table table-responsive">
+					<tr>
+						<td>Date of arrival:</td>
+						<td><?php echo $arrivalDate; ?></td>
+					</tr>
+					<tr>
+						<td>Date of departure:</td>					
+						<td><?php echo $departureDate; ?></td>
+					</tr>
+					<tr>
+						<td>City:</td>
+						<td><?php echo $city; ?></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-6">
+				<div>
+					<h2>Hotel information</h2>
 					<p>Here you will find information about your hotel and room</p>
-				</td>
-			</tr>
-			<tr>
-				<td>Hotel:</td>
-				<td><?php echo $hotelName; ?></td>
-			</tr>
-			<tr>
-				<td>Room:</td>
-				<td><?php echo $roomType; ?></td>
-			</tr>
-			<tr>
-				<td>Room costs:</td>
-				<td><?php echo format_money($roomPrice); ?></td>
-			</tr>
-			<tr class="text-center">
-				<td>
-					<h3>Transport and hotel cost</h3><br>
+				</div>
+				<br>
+				<table class="table table-responsive table-condensed">
+					<tr>
+						<td>Hotel:</td>
+						<td><?php echo $hotelName; ?></td>
+					</tr>
+					<tr>
+						<td>Room:</td>
+						<td><?php echo $roomType; ?></td>
+					</tr>
+					<tr>
+						<td>Room costs:</td>
+						<td><?php echo format_money($roomPrice); ?></td>
+					</tr>
+				</table>
+			</div>
+			<div class="col-md-6 thumb">
+                <div class="hovereffect">
+                    <a class="thumbnail">
+                        <img class="img-responsive" src="<?php echo $standardImage; ?>" alt="<?php echo $hotelName; ?>">
+                    </a>
+                    <div class="overlay">
+                        <h2><?php echo $hotelName; ?></h2>
+                    </div>
+                </div>
+            </div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="text-center">
+					<h2>Transport and hotel cost</h2>
 					<p>Here you will find information about the transport and hotelcost.</p>
-				</td>
-			</tr>
-			<tr>
-				<td>Total hotel costs:</td>
-				<td><?php echo format_money($hotelPrice); ?></td>
-				
-			</tr>
-			<tr>
-				<td>Transportation</td>
-				<td><?php echo $transport_type ?></td>
-			</tr>
-			<tr>
-				<td>Transportation cost</td>
-				<td><?php echo format_money($transportationPrice); ?></td>
-			</tr>
-			<tr class="active">
-				<td>Total:</td>
-				<td><?php echo format_money($totalPrice); ?></td>
-			</tr>
-			</table>
-	
+				</div>
+				<br>
+				<table class="table table-responsive table-condensed">
+					<tr>
+						<td>Total hotel costs:</td>
+						<td><?php echo format_money($hotelPrice); ?></td>
+						
+					</tr>
+					<tr>
+						<td>Transportation</td>
+						<td><?php echo $transport_type ?></td>
+					</tr>
+					<tr>
+						<td>Transportation cost</td>
+						<td><?php echo format_money($transportationPrice); ?></td>
+					</tr>
+					<tr class="active">
+						<td>Total:</td>
+						<td><?php echo format_money($totalPrice); ?></td>
+					</tr>
+				</table>
+			</div>
+		</div>		
+	</div>	
 	<!-- Input form -->
 		<form method="post" action="">
 		<table class="table">
@@ -189,7 +232,7 @@
 		<br>
 		<input type="submit" value="Book now" class="btn btn-success">
 		</form>
-	</div>
+	
 </div>
 </body>
 </html>
