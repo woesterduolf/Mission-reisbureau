0e4d7c8a48a5181c6039cf6a35f4e0008a44e8dc
woesterduolf <rudolfklijnhout@hotmail.com>
2016-11-09 11:43:59 +0100

    Unique Id's

--- hanzeproject/helpers/payment_complete.php
+++ hanzeproject/helpers/payment_complete.php
@@ -1,12 +1,14 @@
 <?php
 
 function payment_complete(){
+	
 	include '../db/connection.php';
 	include '../helpers/functions.php';
+	include '../helpers/create_uniqid.php';
 	session_start();
 	
 	//customer data everything comes from the session except the customerID that is generated here.
-	$customer_id = "";
+	$customer_id = "create_uniqid()";
 	$customer_first_name = $_SESSION['customer_first_name'];
 	$customer_last_name = $_SESSION['customer_last_name'];
 	$customer_adress = $_SESSION['customer_adress'];
@@ -16,7 +18,7 @@ function payment_complete(){
 	$customer_phonenumer = $_SESSION['customer_phonenumber'];
 	
 	//booking data
-	$booking_id = "";
+	$booking_id = "create_uniqid()";
 	$booking_city = $_SESSION['booking_city'];
 	$booking_arrivalDate = $_SESSION['booking_date_of_arrival'];
 	$booking_departureDate = $_SESSION['booking_date_of_departure'];
@@ -25,18 +27,20 @@ function payment_complete(){
 	
 	
 	//bus data
-	$bus_resevation_id = "";
+	$bus_resevation_id = "create_uniqid()";
 	$bus_boarding_point = $_SESSION['bus_boarding_point'];
 	$bus_price = $_SESSION['bus_price'];
 	$bus_boarding_point_return = $_SESSION['$bus_boarding_point_return'];
 	$booking_id;
 	
 	// fligth data
-	$fligth_id = "";
+	$fligth_id = "create_uniqid()";
 	$flight_price = $_SESSION['flight_Price'];
 	$flight_departure_time = $_SESSION['flight_departure_time'];
 	$flight_arrival_time = $_SESSION['flight_arrival_time'];
 	$booking_id;
+	
+	
 	
 }	
 ?>
