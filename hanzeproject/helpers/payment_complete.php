303f544ba8183acd6bac338169cca4cc2dac1f2e
woesterduolf <rudolfklijnhout@hotmail.com>
2016-11-09 11:02:44 +0100

    alle session variable

--- hanzeproject/helpers/payment_complete.php
+++ hanzeproject/helpers/payment_complete.php
@@ -16,7 +16,7 @@ function payment_complete(){
 	$customer_phonenumer = $_SESSION['customer_phonenumber'];
 	
 	//booking data
-	$bookingID = "";
+	$booking_id = "";
 	$booking_city = $_SESSION['booking_city'];
 	$booking_arrivalDate = $_SESSION['booking_date_of_arrival'];
 	$booking_departureDate = $_SESSION['booking_date_of_departure'];
@@ -29,11 +29,15 @@ function payment_complete(){
 	$bus_boarding_point = $_SESSION['bus_boarding_point'];
 	$bus_price = $_SESSION['bus_price'];
 	$bus_boarding_point_return = $_SESSION['$bus_boarding_point_return'];
-	$bookingID;
+	$booking_id;
 	
 	// fligth data
 	$fligth_id = "";
-	$flight_price = $_SESSiON['flight_Price'];
+	$fligth_seat = $_SESSION['fligth_seat'];
+	$flight_price = $_SESSION['flight_Price'];
+	$flight_departure_time = $_SESSION['flight_departure_time'];
+	$flight_arrival_time = $_SESSION['flight_arrival_time'];
+	$booking_id;
 	
 }	
 ?>
