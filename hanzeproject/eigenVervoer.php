5550c102ab5c417b43c9d1406282e8d5544cc21a
woesterduolf <rudolfklijnhout@hotmail.com>
2016-11-10 16:47:45 +0100

    eigen vervoer met 10 seconden delay

--- hanzeproject/eigenVervoer.php
+++ hanzeproject/eigenVervoer.php
@@ -1,10 +1,13 @@
 
 <?php
+	session_start();
 	if(isset($_GET['pickedRoom'])){
 		$_SESSION['room_id'] = $_GET['pickedRoom'];
 	}else{
 		header("refresh:0;url=pickRoom.php");
 	}
+	var_dump($_GET['pickedRoom']);
 	echo "GODVERDOMME RICARDO NIET NOG EEN PAGINA!";
+	header("refresh:10;url=includes/booking.php");
 ?>
 
