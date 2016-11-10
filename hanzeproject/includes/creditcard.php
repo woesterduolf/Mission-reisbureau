0fa7bf108df2be8f2d5fe8692b92355358c617b7
Tobias <tobiasschiphorst@live.nl>
2016-11-10 13:18:45 +0100

    Overwrite error

--- hanzeproject/includes/creditcard.php
+++ hanzeproject/includes/creditcard.php
@@ -1,3 +1,17 @@
+<?php
+
+	include_once '../db/connection.php';
+	include_once '../helpers/functions.php';
+	include_once '../helpers/payment_complete.php';
+	$cardholder = $cardnumber = $expiredate = $cvvcode
+	if (!empty($_POST)) {
+		if (empty($_POST['name-cardholder'])) {
+			$
+		}
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,19 +32,26 @@
 			<form method="POST" action="">
 				<div class="form-group">
 					<label for="mr-name-cardholder">Name cardholder</label>
-					<input type="text" name="name-cardholder" class="form-control" required="true" value="<?php echo $_POST['name-cardholder'] ?>" id="mr-name-cardholder">
+					<input type="text" name="name-cardholder" class="form-control" required="true" value="<?php echo $cardholder; ?>" id="mr-name-cardholder">
 				</div>
 				<div class="form-group">
 					<label for="mr-creditcard-number">Creditcard number</label>
-					<input type="text" name="cardnumber" class="form-control" required="true" value="<?php echo $_POST['cardnumber'] ?>" id="mr-creditcard-number">
+					<input type="text" name="cardnumber" class="form-control" required="true" value="<?php echo $cardnumber; ?>" id="mr-creditcard-number">
 				</div>	
 				<div class="form-group">
 					<label for="mr-expiredate">Expire date</label>
-					<input type="text" name="expiredate" class="form-control" required="true" value="<?php echo $_POST['expiredate'] ?>" id="mr-expiredate">
+					<input type="text" name="expiredate" class="form-control" required="true" value="<?php echo $ ?>" id="mr-expiredate">
 				</div>
 				<div class="form-group">
 					<label for="mr-cvv">CVV-code</label>
 					<input type="text" name="cvv-code" class="form-control" required="true" value="<?php echo $_POST['cvv-code'] ?>" id="mr-cvv">
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="text-center">
+							<button type="submit" value="Complete payment" class="btn btn-succes btn-lg">Complete payment</button>
+						</div>
+					</div>
 				</div>
 			</form>
 		</div>
