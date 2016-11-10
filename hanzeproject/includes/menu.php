ca9bacc5141fa69e96195375498daa30a58cb47c
HeHu IT Services <henkiehulsie@gmail.com>
2016-11-10 15:24:39 +0100

    login link gemaakt

--- hanzeproject/includes/menu.php
+++ hanzeproject/includes/menu.php
@@ -1,9 +1,9 @@
 <ul class="cl-effect-1">
-    <li><a class="scroll" href="#home">HOME</a></li>
-    <li><a class="scroll" href="#brief">BRIEF</a></li>
+    <li><a class="scroll" href="login/index.php">LOGIN</a></li>
+    <!--<li><a class="scroll" href="#brief">BRIEF</a></li>
     <li><a class="scroll" href="#features">FEATURES</a></li>
     <li><a class="scroll" href="#screenshots">TOURS</a></li>
     <li><a class="scroll" href="#testimonial">TESTIMONIAL</a></li>
-    <li><a class="scroll" href="#contact">CONTACT</a></li>
+    <li><a class="scroll" href="#contact">CONTACT</a></li>-->
 
 </ul>
